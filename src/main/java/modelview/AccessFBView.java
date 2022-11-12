8366a5e5ad8bc155afc3d866a7d227bf89144f71
cgonc <cgonc@DESKTOP-2BBBQBI.home>
2022-11-11 23:42:22 -0500

    push to repo

--- src/main/java/modelview/AccessFBView.java
+++ src/main/java/modelview/AccessFBView.java
@@ -8,30 +8,39 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.OidcProviderConfig.CreateRequest;
+import com.google.firebase.auth.UserRecord;
 import com.mycompany.mvvmexample.FirestoreContext;
 import com.mycompany.mvvmexample.FirestoreContext;
 import com.mycompany.mvvmexample.FirestoreContext;
 import com.mycompany.mvvmexample.FirestoreContext;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import models.Person;
 
 public class AccessFBView {
 
- 
-     @FXML
+    @FXML
     private TextField nameField;
     @FXML
     private TextField majorField;
@@ -42,10 +51,18 @@ public class AccessFBView {
     @FXML
     private Button readButton;
     @FXML
-    private TextArea outputField;
-     private boolean key;
+    private TableView<Person> outputField;
+    @FXML
+    private TableColumn<Person,String> namecol;
+    @FXML
+    private TableColumn<Person,String> majorcol;
+    @FXML
+    private TableColumn<Person,Integer> agecol;
+
+    private boolean key;
     private ObservableList<Person> listOfUsers = FXCollections.observableArrayList();
     private Person person;
+
     public ObservableList<Person> getListOfUsers() {
         return listOfUsers;
     }
@@ -56,16 +73,25 @@ public class AccessFBView {
         nameField.textProperty().bindBidirectional(accessDataViewModel.userNameProperty());
         majorField.textProperty().bindBidirectional(accessDataViewModel.userMajorProperty());
         writeButton.disableProperty().bind(accessDataViewModel.isWritePossibleProperty().not());
+        namecol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        majorcol.setCellValueFactory(new PropertyValueFactory<>("major"));
+        namecol.setCellValueFactory(new PropertyValueFactory<>("age"));
+
     }
 
     @FXML
     private void addRecord(ActionEvent event) {
         addData();
+        nameField.setText("");
+        majorField.setText("");
+        ageField.setText("");
     }
 
-        @FXML
+    @FXML
     private void writeRecord(ActionEvent event) {
+
         readFirebase();
+        outputField.setItems(listOfUsers);
     }
     
     public void addData() {
@@ -79,43 +105,36 @@ public class AccessFBView {
         //asynchronously write data
         ApiFuture<WriteResult> result = docRef.set(data);
     }
-    
-        public boolean readFirebase()
-         {
-             key = false;
+
+    public boolean readFirebase() {
+        key = false;
 
         //asynchronously retrieve all documents
-        ApiFuture<QuerySnapshot> future =  App.fstore.collection("References").get();
+        ApiFuture<QuerySnapshot> future = App.fstore.collection("References").get();
         // future.get() blocks on response
         List<QueryDocumentSnapshot> documents;
-        try 
-        {
+        try {
             documents = future.get().getDocuments();
-            if(documents.size()>0)
-            {
+            if (documents.size() > 0) {
                 System.out.println("Outing....");
-                for (QueryDocumentSnapshot document : documents) 
-                {
-                    outputField.setText(outputField.getText()+ document.getData().get("Name")+ " , Major: "+
-                            document.getData().get("Major")+ " , Age: "+
-                            document.getData().get("Age")+ " \n ");
-                    System.out.println(document.getId() + " => " + document.getData().get("Name"));
-                    person  = new Person(String.valueOf(document.getData().get("Name")), 
+                for (QueryDocumentSnapshot document : documents) {
+                    
+                    /*outputField.setText(outputField.getText() + document.getData().get("Name") + " , Major: "
+                            + document.getData().get("Major") + " , Age: "
+                            + document.getData().get("Age") + " \n ");
+                    System.out.println(document.getId() + " => " + document.getData().get("Name"));*/
+                    person = new Person(String.valueOf(document.getData().get("Name")),
                             document.getData().get("Major").toString(),
                             Integer.parseInt(document.getData().get("Age").toString()));
                     listOfUsers.add(person);
                 }
+            } else {
+                System.out.println("No data");
             }
-            else
-            {
-               System.out.println("No data"); 
-            }
-            key=true;
-            
-        }
-        catch (InterruptedException | ExecutionException ex) 
-        {
-             ex.printStackTrace();
+            key = true;
+
+        } catch (InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
         }
         return key;
     }
